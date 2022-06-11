c5ece49068f802859ab484fc7647556e76d62757
酸菜 <suancai@admobile.top>
2022-06-11 17:22:31 +0800

    [Style] git rm -r --cached Pods

--- Example/Pods/Target Support Files/Pods-lsj-PrivateProtocolAlert_Example/Pods-lsj-PrivateProtocolAlert_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_lsj_PrivateProtocolAlert_Example : NSObject
-@end
-@implementation PodsDummy_Pods_lsj_PrivateProtocolAlert_Example
-@end
--- Example/Pods/Target Support Files/lsj-PrivateProtocolAlert/lsj-PrivateProtocolAlert-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_lsj_PrivateProtocolAlert : NSObject
-@end
-@implementation PodsDummy_lsj_PrivateProtocolAlert
-@end
